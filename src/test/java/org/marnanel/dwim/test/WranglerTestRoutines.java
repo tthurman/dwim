83b8abc7ebb035d8b398619733d1a1877e2cf9e5
Thomas Thurman <thomas@thurman.uk>
2016-08-03 02:37:22 +0100

    more test routines, but still just sketching

--- src/test/java/org/marnanel/dwim/test/WranglerTestRoutines.java
+++ src/test/java/org/marnanel/dwim/test/WranglerTestRoutines.java
@@ -1,11 +1,19 @@
 package org.marnanel.dwim.test;
 
+import org.marnanel.dwim.Wrangler;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import android.util.Log;
+import android.os.Bundle;
 
 public class WranglerTestRoutines {
-	public static void wranglerTest(String source, String expect) {
 
-	    assert(source=="foo");
-	}
+        static final String TAG = "WranglerTestRoutines";
+
+        public static void wranglerTest(String sourceText, String expectText) {
+
+                Bundle result = Wrangler.parse(sourceText);
+                Log.w(TAG, result.toString());
+                assert(false);
+        }
 }
