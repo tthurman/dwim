198924ca3b5bcb0f36e9efa75cbcc2f405fe30ce
Thomas Thurman <thomas@thurman.uk>
2016-08-21 20:10:13 +0100

    first checkin of wrangler

---
+++ src/main/java/org/marnanel/dwim/Wrangler.java
@@ -0,0 +1,164 @@
+package org.marnanel.dwim;
+
+/**
+ * This parses the scraped HTML.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.util.Log;
+import android.os.Bundle;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.safety.Whitelist;
+
+public class Wrangler {
+
+        public static final String TAG = "Wrangler";
+
+        public static final String PAGE_TYPE = "pagetype";
+        public static final String PAGE_TYPE_TIMELINE = "timeline";
+        public static final String PAGE_TYPE_LOGIN = "login";
+        public static final String PAGE_TYPE_UNKNOWN = "unknown";
+
+        protected Wrangler() {
+        }
+
+        protected static Bundle wrangleTimeline(Document doc) {
+
+                Elements sourceEntries = doc.select(".entry-wrapper");
+
+                if (sourceEntries.isEmpty()) {
+
+                        return null;
+                }
+                ArrayList<Bundle> resultEntries = new ArrayList<Bundle>();
+                
+                for (Element sourceEntry: sourceEntries) {
+
+                        Bundle resultEntry = new Bundle();
+
+                        resultEntry.putString("sec", sourceEntry.className());
+                        for (String sourceEntryClass: sourceEntry.classNames()) {
+                                if (sourceEntryClass.startsWith("has-") ||
+                                    sourceEntryClass.startsWith("entry-")) {
+                                        continue;
+                                }
+                        
+                                int lastHyphen = sourceEntryClass.lastIndexOf('-');
+
+                                if (lastHyphen==-1) {
+                                        continue;
+                                }
+
+                                String field = sourceEntryClass.substring(0,lastHyphen);
+                                String value = sourceEntryClass.substring(lastHyphen+1);
+
+                                resultEntry.putString(field, value);
+                        }
+
+                       Element content = sourceEntry.select(".entry-content").first();
+                        if (content==null) {
+                                resultEntry.putString("content", "");
+                        } else {
+                                Whitelist whitelist = Whitelist.relaxed();
+                                String cleanedHTML =
+                                        Jsoup.clean(content.html(),
+                                                Whitelist.relaxed());
+                                resultEntry.putString("content", cleanedHTML);
+                        }
+
+                        // TODO: images
+                        // Not really our problem atm, but when we handle
+                        // offline access, we'll need to preload.
+
+                        Element title = sourceEntry.select(".entry-title a").first();
+                        if (title==null) {
+                                // XXX tbh an entry without a permalink is
+                                // probably not very useful; maybe we should
+                                // discard it.
+                                resultEntry.putString("title", "");
+                                resultEntry.putString("permalink", "");
+                        } else {
+                                resultEntry.putString("title", title.attr("title"));
+                                resultEntry.putString("permalink", title.attr("href"));
+                        }
+
+
+                        // TODO: tags
+
+                        // TODO: userpic
+
+                        resultEntries.add(resultEntry);
+                }
+
+                Bundle result = new Bundle();
+                result.putString(PAGE_TYPE, PAGE_TYPE_TIMELINE);
+                result.putParcelableArrayList("entries", resultEntries);
+
+                return result;
+        }
+
+        protected static Bundle wrangleTaglist(Document doc) {
+                return null;
+        }
+
+        protected static Bundle wrangleIcons(Document doc) {
+                return null;
+        }
+
+        protected static Bundle wrangleLogin(Document doc) {
+                if (doc.select(".lj_login_form").isEmpty()) {
+                        return null;
+                }
+
+                Bundle result = new Bundle();
+
+                for (Element e: doc.select("input")) {
+                        result.putString(
+                                e.attr("name"),
+                                e.attr("value"));
+                }
+
+                // Add system fields last, so the parsed HTML
+                // can't override them.
+                result.putString(PAGE_TYPE, PAGE_TYPE_LOGIN);
+
+                return result;
+        }
+
+        protected static Bundle wrangleUnknown(Document doc) {
+                Bundle result = new Bundle();
+                result.putString(PAGE_TYPE, PAGE_TYPE_UNKNOWN);
+
+                return result;
+        }
+
+        public static Bundle parse(String html) {
+
+                Bundle result;
+                Document doc = Jsoup.parse(html, "UTF-8");
+
+                // This isn't elegant but I've messed
+                // around with trying to build a list
+                // of inner classes within a static method
+                // for far too long. Why did Java take
+                // so long to get method references?
+
+                result = wrangleTimeline(doc);
+                if (result!=null) { return result; }
+
+                result = wrangleIcons(doc);
+                if (result!=null) { return result; }
+
+                result = wrangleTaglist(doc);
+                if (result!=null) { return result; }
+
+                result = wrangleLogin(doc);
+                if (result!=null) { return result; }
+
+                return wrangleUnknown(doc);
+        }
+}
